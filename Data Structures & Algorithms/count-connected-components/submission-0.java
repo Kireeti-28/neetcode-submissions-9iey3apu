ebd48cbc70e32b54541ce7894da284b5d1fe7452
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-05-11 20:55:00 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.java
@@ -0,0 +1,56 @@
+class Solution {
+    public Map<Integer, List<Integer>> buildAdjacencyList(int n, int[][] edges) {
+        Map<Integer, List<Integer>> adjacencyList = new HashMap<>();
+
+        for (int[] edge: edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (adjacencyList.containsKey(u)) {
+                var neighbors = adjacencyList.get(u);
+                neighbors.add(v);
+            } else {
+                var neighbors = new ArrayList();
+                neighbors.add(v);
+                adjacencyList.put(u, neighbors);
+            }
+
+            if (adjacencyList.containsKey(v)) {
+                var neighbors = adjacencyList.get(v);
+                neighbors.add(u);
+            } else {
+                var neighbors = new ArrayList();
+                neighbors.add(u);
+                adjacencyList.put(v, neighbors);
+            }
+        }
+
+        return adjacencyList;
+    }
+
+    public void traverseComponent(int node, Set<Integer> visited, Map<Integer, List<Integer>> adjList) {
+        if (visited.contains(node) || !adjList.containsKey(node) ) {
+            return;
+        }
+
+        visited.add(node);
+
+        for (Integer neighbor: adjList.get(node)) {
+            traverseComponent(neighbor, visited, adjList);
+        }
+    }
+
+    public int countComponents(int n, int[][] edges) {
+        var adjList = buildAdjacencyList(n, edges);
+        var visited = new HashSet<Integer>();
+        var components = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited.contains(i)) {
+                traverseComponent(i, visited, adjList);
+                components++;
+            }
+        }
+
+        return components;
+    }
+}
