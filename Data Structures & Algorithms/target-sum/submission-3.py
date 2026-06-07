2a3e91f57c2332903a8b2fa4c8e5327dfdb2cd75
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-06-07 17:54:12 +0530

    Add: target-sum - submission-3

---
+++ Data Structures & Algorithms/target-sum/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        def tabulation():
+            n = len(nums)
+            dp = [defaultdict(int) for _ in range(n + 1)]
+            dp[0][0] = 0
+
+            for i in range(n):
+                for total, count in dp[i].items():
+                    dp[i + 1][total + nums[i]] += count
+                    dp[i + 1][total - nums[i]] += count
+            
+            return dp[n][target]
+        
+        return tabulation()
+
+        memo = {}
+        def dfs(i, cur_sum):
+            if i == len(nums):
+                return 1 if cur_sum == target else 0
+            
+            if (i, cur_sum) in memo:
+                return memo[(i, cur_sum)]
+
+            memo[(i, cur_sum)] = dfs(i + 1, cur_sum + nums[i]) + dfs(i + 1, cur_sum - nums[i])
+            return memo[(i, cur_sum)]
+        
+        return dfs(0, 0)
