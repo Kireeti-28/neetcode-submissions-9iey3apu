ceee314907293558abc49916c02a99414f85f7ff
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-06-21 20:39:19 +0530

    Add: sqrtx - submission-7

---
+++ Data Structures & Algorithms/sqrtx/submission-7.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l = 1;
+        int r = x;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            long num = (long) m * m;
+            if (mum == x) {
+                return m;
+            }
+
+            if (num > x) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return r;
+    }
+}
