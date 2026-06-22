90d46e40b7aa43169a85fe1a91b5f70be4443cae
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-06-22 20:52:35 +0530

    Add: largest-3-same-digit-number-in-string - submission-0

---
+++ Data Structures & Algorithms/largest-3-same-digit-number-in-string/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        goodInts = []
+
+        def isAllSame(s):
+            l = 0
+            r = len(s) - 1
+
+            while l <= r:
+                if s[l] != s[r]:
+                    return False
+                
+                l += 1
+                r -= 1
+            
+            return True
+
+        for i in range(len(num)):
+            for j in range(i, len(num) + 1):
+                if (j - i) == 3 and isAllSame(num[i:j]):
+                    goodInts.append(num[i:j])
+                    
+        return max(goodInts) if len(goodInts) > 0 else ""
+                    
