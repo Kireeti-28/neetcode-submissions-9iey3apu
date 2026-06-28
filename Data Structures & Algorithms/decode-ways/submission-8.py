89614e16d4881c5819ce3c8f0fc197803300d46e
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-06-28 20:48:47 +0530

    Add: decode-ways - submission-8

---
+++ Data Structures & Algorithms/decode-ways/submission-8.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+        
+        def dfs(i):
+            if i == len(s):
+                return 1
+            
+            if i in memo:
+                return memo[i]
+
+            if s[i] == '0': return 0
+
+            res = dfs(i + 1)
+
+            if (i + 1) < len(s):
+                two_digit = int(s[i] + s[i+1])
+                if 10 <= two_digit <= 26:
+                    res += dfs(i + 2)
+            # print(memo)
+            memo[i] = res
+            return memo[i]
+        ans = dfs(0)
+        print(memo)
+        return ans
+            
