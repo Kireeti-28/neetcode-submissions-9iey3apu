b7638c76d84cfdf087e3d71d931237a5117a9a99
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-07-02 07:13:22 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap();
+
+        for (int num: nums) {
+            if (freq.containsKey(num)) {
+                return true;
+            }
+
+            freq.put(num, 1);
+        }
+
+        return false;
+    }
+}
