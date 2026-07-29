5a33a8fd66bcfa8db7ec5f6ddfa86b73ac904d8f
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-07-29 09:36:11 +0530

    Add: unique-paths-ii - submission-3

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        dp[m - 1][n - 1] = 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if ((i == m - 1 && j == n - 1) || obstacleGrid[i][j] == 1) {
+                    continue;
+                }
+
+                int right = (j + 1 < n) ? dp[i][j + 1] : 0;
+                int down = (i + 1 < m) ? dp[i + 1][j] : 0;
+
+                dp[i][j] = right + down;
+            }
+        }
+
+        return dp[0][0];
+    }
+}
