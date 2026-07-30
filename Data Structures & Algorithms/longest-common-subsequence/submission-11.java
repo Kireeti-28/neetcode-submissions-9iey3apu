dfdf35037ad3a8e89f3b937fd2fe67b54e5dccf1
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-07-30 20:46:48 +0530

    Add: longest-common-subsequence - submission-11

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-11.java
@@ -0,0 +1,17 @@
+class Solution {
+    private int helper(int i, int j, String text1, String text2) {
+        if (i >= text1.length() || j >= text2.length()) return 0;
+
+        int res = 0;
+        if (text1.charAt(i) == text2.charAt(j)) {
+            res = 1 + helper(i + 1, j + 1, text1, text2);
+        } else {
+            res = Math.max(helper(i, j + 1, text1, text2), helper(i + 1, j, text1, text2));
+        }
+
+        return res;
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        return helper(0, 0, text1, text2);
+    }
+}
