0cf8040e4924a8f243fe54f2d83fd1f21b234c58
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-08-03 09:05:25 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = [[0, 0] for _ in range(len(prices) + 2)]
+        print(dp)
+
+        for i in range(len(prices) - 1, -1, -1):
+            for choice in [0, 1]: # 0 for buy & 1 for sell
+                if choice == 0:
+                    dp[i][0] = max(dp[i + 1][0], dp[i + 1][1] - prices[i])
+                else:
+                    dp[i][1] = max(dp[i + 1][1], dp[i + 2][0] + prices[i])
+        
+        print(dp)
+         
+        return dp[0][0]
+
