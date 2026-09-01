dbe18fd4e45fd3bd8c1d2a85241688cdc614eb3a
Kireeti <97286047+Kireeti-28@users.noreply.github.com>
2026-09-01 09:22:26 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.java
@@ -0,0 +1,38 @@
+class MyHashSet {
+    private int[] bucket;
+    private int CAP = 10000;
+
+    public MyHashSet() {
+        this.bucket = new int[CAP];
+        Arrays.fill(bucket, -1);
+    }
+
+    private int hash(int key) {
+        return key % CAP;
+    }
+    
+    public void add(int key) {
+        int idx = hash(key);
+        bucket[idx] = key;
+    }
+    
+    public void remove(int key) {
+        int idx = hash(key);
+        if (bucket[idx] != -1) {
+            bucket[idx] = -1;
+        }
+    }
+    
+    public boolean contains(int key) {
+        int idx = hash(key);
+        return bucket[idx] != -1;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
